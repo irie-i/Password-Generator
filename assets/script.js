34f5a5f50fa994916b4644cfa089873dc624b1df
Afshin M <a.mostofi17@gmail.com>
2022-10-17 22:34:22 -0700

    Updated JS, No longer shows undefinded when no option is chosen

--- assets/script.js
+++ assets/script.js
@@ -6,7 +6,7 @@ var generateBtn = document.querySelector("#generate");
 const lowerList = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const upperList = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const charList = ["!", "#", "$", "%", "&", "*", "+", "-", ".", "^", "_"];
-const numberList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+const numberList = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 
 // Write password to the #password input
@@ -27,20 +27,18 @@ function generatePassword() {
   var passLength = window.prompt("Password Length (8 -128 characters)");
   console.log("Passweord Length: " + passLength);
   if (passLength >= 8 && passLength <= 128) {
-    window.alert("Your number is between 8 and 128! " + "You chose: " + passLength);
+    window.alert("You chose: " + passLength);
   }
 
   else {
-    window.alert("Please choose a number between 8-128");
-    return;
+    return "Please choose a number between 8 - 128";
   }
   //Uppercase
   var uCase = window.confirm("Include uppercase?");
-  console.log("Uppercase: " + uCase)
+  console.log("Uppercase: " + uCase);
   //Lowercase
   var lCase = window.confirm("Include lowercase?");
   console.log("Lowercase: " + lCase);
-
   //Numbers selection
   var numChoice = window.confirm("Include numbers?");
   console.log("Numbers Included " + numChoice);
@@ -61,12 +59,15 @@ function generatePassword() {
   if (specChar) {
     pwOptions = pwOptions.concat(charList);
   }
-
+ if (uCase == false && lCase == false && specChar == false && numChoice == false) {
+    console.log("One of the character options must be selected");
+    return "One of the character options must be selected. Please try again.";
+  }
   // Creating loop that passes through as mentioned in Password Length question allowing to generate a password with the selected critera)c
   var randomPw = "";
   for (var i = 0; i <= passLength; i++) {
-   randomPw = randomPw + pwOptions[Math.floor(Math.random() * pwOptions.length)];
-    console.log(randomPw)
+  randomPw = randomPw + pwOptions[Math.floor(Math.random() * pwOptions.length)];
+  console.log(randomPw);
 
   }
   
